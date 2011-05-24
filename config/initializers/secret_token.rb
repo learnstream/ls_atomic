15d7fc72135c315c38bb5351720d59f8abb0ba63
Ryan Muller <cognitionmachine@gmail.com>
2011-05-24 10:54:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LsAtomic::Application.config.secret_token = '9a2a0048704204c2d992ba201db470f85064068c2ff116fbe5a36f9edde6d7ce6b2a1e14f9d65454c4def7d12b778bee25ee3258298075d98875acb6d6e85891'
