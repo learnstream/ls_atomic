4db8ef34019300b7fea07188f2fdc1f5c0698efa
Ryan Muller <cognitionmachine@gmail.com>
2011-05-24 15:23:22 -0700

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 LsAtomic::Application.routes.draw do |map|
-  get "pages/home"
-
-  get "pages/about"
-
-  get "pages/help"
-
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  
+  root :to => 'pages#home'
   resource :user_session
   resources :users
 end
