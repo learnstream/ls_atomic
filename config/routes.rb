0b5bd6986a2378838ddab347efd85dc2b30431c2
Ryan Muller <cognitionmachine@gmail.com>
2011-05-24 15:41:49 -0700

    added some paths for homepage and signing up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 LsAtomic::Application.routes.draw do |map|
+  get "users/new"
+  match '/signup', :to => 'users#new'
+
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   
   root :to => 'pages#home'
+
   resource :user_session
   resources :users
 end
