6a49e1fa8c25fb2c9e70baa4b7a828b8addf71e4
Neal Pisenti <npisenti@gmail.com>
2011-05-26 14:11:01 -0700

    Problem Models added, tests for those added

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,2 +1,38 @@
 class ProblemsController < ApplicationController
+  before_filter :authenticate,
+
+  def create
+    course_id = params[:problem][:course_id]
+    if course_id.nil?
+      flash[:error] = "If you know what's good, add a course. I have no idea what you did"
+      redirect_to root_path
+    else
+      course = Course.find(course_id)
+      @problem = course.problems.build(params[:problem])
+    end
+
+    if @problem.save
+      flash[:success] = "Problem created!"
+      redirect_to problem_path
+    else
+      flash[:error] = "There was a problem saving. Try again, or not."
+      redirect_to course_path(course_id)
+    end   
+  end
+
+  #def new
+  #end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
 end
