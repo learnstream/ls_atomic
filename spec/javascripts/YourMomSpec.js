9f2f9e676c079f1cbc20a5192770c57dab4c3c20
Ryan Muller <cognitionmachine@gmail.com>
2011-05-30 17:01:34 -0700

    playing with jasmine

---
+++ spec/javascripts/YourMomSpec.js
@@ -0,0 +1,10 @@
+describe("YourMom", function() {
+    it("is fat", function() {
+      expect(YourMom.weight).toBeGreaterThan(500);
+      });
+    it("has a good error message", function() {  
+      loadFixtures("order_form.html");  
+      $("#card_number_error").text("Your mom is fat");
+      expect($("#card_number_error")).toHaveText("fat");
+      });  
+    });
