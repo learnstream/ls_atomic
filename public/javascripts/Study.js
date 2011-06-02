3a7c4b7c3112ed56493bd3f3a24f9347d7b117a2
Neal Pisenti <npisenti@gmail.com>
2011-06-01 17:41:12 -0700

    added video links to steps with video in problem study interface

--- public/javascripts/Study.js
+++ public/javascripts/Study.js
@@ -8,15 +8,26 @@ $(document).ready(function() {
       $("#rate-button-panel").show();
       $("#show-answer").hide();
       $("#study-item").text("What did you think?");
-      $.get("/steps/" + data.id + "/help/", function(comp_data) {
-        if (comp_data.length > 0) {
+      $.get("/steps/" + data.id + "/help/", function(data) {
+
+        var video_data = data.videos;
+        var comp_data = data.components;
+
+      
+        if ((video_data.length > 0) || (comp_data.length > 0)) {
           $("#help").show();
+        }
+
+        for (i = 0; i < video_data.length; i++) {
+          video = video_data[i];
+          var old_html = $("#help-items-videos").html();
+          $("#help-items-videos").html(old_html + '<li>' + video.name + ' | <a href = "' + video.url + '">Watch</a><br />' + video.description + '</li>');
         }
 
         for (i = 0; i < comp_data.length; i++) {
           comp = comp_data[i];
-          var old_html = $("#help-items").html();
-          $("#help-items").html(old_html + '<li><a href="' + comp.path + '">' + comp.name + '</a></li>');
+          var old_html = $("#help-items-components").html();
+          $("#help-items-components").html(old_html + '<li><a href="' + comp.path + '">' + comp.name + '</a></li>');
         } 
         });
     });
