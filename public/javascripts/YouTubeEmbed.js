c7efef1aae45f54b8bf1bd2f5058c660429a6a8c
Ryan Muller <cognitionmachine@gmail.com>
2011-06-02 21:29:52 -0700

    got modal video display working

--- public/javascripts/YouTubeEmbed.js
+++ public/javascripts/YouTubeEmbed.js
@@ -1,67 +1,90 @@
-var current_end_time;
-var ytplayer;
-var ytTimer;
-
+var current_end_time = 0; 
+var ytplayer = null; 
+var ytTimer = null;
 
 // Gets the Youtube ID from a URL; name => 'v' for youtube URL format
-function getYoutubeID(url, name) {
-    var urlparts = url.split('?');
-    if (urlparts.length > 1) {
-        var parameters = urlparts[1].split('&');
-        for (var i = 0; i < parameters.length; i++) {
-            var paramparts = parameters[i].split('=');
-            if (paramparts.length > 1 && unescape(paramparts[0]) == name) {
-                return unescape(paramparts[1]);
-            }
-        }
+getYoutubeID = function(url, name) {
+  var urlparts = url.split('?');
+  if (urlparts.length > 1) {
+    var parameters = urlparts[1].split('&');
+    for (var i = 0; i < parameters.length; i++) {
+      var paramparts = parameters[i].split('=');
+      if (paramparts.length > 1 && unescape(paramparts[0]) == name) {
+        return unescape(paramparts[1]);
+      }
     }
-    return null;
+  }
+  return null;
+};
+
+
+
+createYoutubeURL = function(videoID, start_time) {
+  return 'http://www.youtube.com/v/' + videoID + 
+    '&enablejsapi=1&playerapiid=player1&autoplay=1&start=' + start_time;
 }
+
+
+constructColorBoxLink = function(video, index) {
+  var videoID = getYoutubeID(video.url, 'v');
+  var old_html = $("#help-items-videos").html();
+  return '<li>' + video.name + ' | ' +
+    '<a id="video-watch-link-' + index +'" ' + 
+    'class="color-box-link" ' +
+    'href="#"' +
+    'data-url="' + videoID + '" ' +
+    'data-start-time="' + video.start_time + '" ' +
+    'data-end-time="' + video.end_time + '" ' +
+    '>' + 'Watch' + '</a></li>';
+};
+
+
+constructVideoWatchLink = function(video, index) {
+  var videoID = getYoutubeID(video.url, 'v');
+  var old_html = $("#help-items-videos").html();
+
+  return '<li>' + video.name + ' | ' + 
+    '<a class="youtube" href="#" ' + 
+    'data-url="' + videoID + '" ' +
+    'data-start-time="' + video.start_time + '" ' +
+    'data-end-time="' + video.end_time + '" ' +
+    'id="video-watch-link-' + index + '">Watch</a><br />'
+    + video.description + '</li>';
+};
 
 
 // Loads the youtube player into the div with name 'embed_area'
-function load_youtube_player(video_url, start_time, end_time, embed_area) {
-  var videoID = getYoutubeID(video_url, 'v');
-  alert(start_time + 3);
+loadYouTubePlayer = function(video_id, start_time, end_time, embed_area) {
   var params = { allowScriptAccess: "always" };
   var atts = { id: "ytPlayer" };
-  swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "&enablejsapi=1&playerapiid=player1&start=" + start_time, embed_area, "480", "295", "8", null, null, params, atts); 
-  if (! end_time == 0){
-  current_end_time = end_time;
-  ytTimer = window.setInterval(check_end_time, 1000);
+  swfobject.embedSWF("http://www.youtube.com/v/" + video_id + "&enablejsapi=1&playerapiid=player1&autoplay=1&start=" + start_time, embed_area, "480", "295", "8", null, null, params, atts); 
+};
+
+function loadAndPlayVideo(video_id, start_time, end_time, embed_area) {
+  if (ytplayer == null) 
+   loadYouTubePlayer(video_id, start_time, end_time, embed_area);
+  else  {
+    ytplayer.loadVideoById(video_id, start_time);
   }
-}
-
-
-//Called after successful youtube player load
-function onYouTubePlayerReady(playerId) {
-    alert('ytready');
-      ytplayer = document.getElementById("ytPlayer");
-    }
-
-
-
-// plays the video in the video player ytplayer
-function play_video(video_url, start_time, end_time) {
-  var videoID = getYoutubeID(video_url, 'v');
-  ytplayer.loadVideoById(videoID, start_time)
-
-  if (! end_time == 0){
+  
+  if (end_time != 0) {
     current_end_time = end_time;
     clearInterval(ytTimer);
     ytTimer = window.setInterval(check_end_time, 1000);
   }
-}
+}              
 
+//Called after successful youtube player load
+function onYouTubePlayerReady() {
+  ytplayer = document.getElementById("ytPlayer");
+}
 
 // Checks to see if current video is past the designated 'end time'
 // This function should be called every second by ytTimer, after playing a video
 function check_end_time() {
   var time = ytplayer.getCurrentTime();
-  if (time > current_end_time){
+  if (current_end_time != 0 && time > current_end_time){
     ytplayer.pauseVideo();
     clearInterval(ytTimer);
   }
 }
-
-
