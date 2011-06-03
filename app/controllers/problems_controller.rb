2880d6f2cdab099dc49cdadcbaa9d24d852e5728
Neal Pisenti <npisenti@gmail.com>
2011-06-02 19:54:19 -0700

    added new problem form with tex input

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -23,11 +23,44 @@ class ProblemsController < ApplicationController
     end
    end
 
+  def tex_create
+    course = Course.find(params[:problem][:course_id])
+    if course.nil? 
+      flash[:error] = "You're trying to add to a course that doesn't exist"
+      redirect_to root_path
+      return
+    end
+    
+    @problem = course.problems.build(params[:problem])
+    @problem.name = find_name(params[:problem][:statement])   
+    @problem.statement = find_statement(params[:problem][:statement])
+   
+    @problem.save #may want to check this
+    array = params[:problem][:statement].scan(/\\begin{step}(.*?)\\end{step}/im)
+      array.each {|step|
+        stepText = step.first.chomp 
+        @problem.steps.create!(:text => stepText, :order_number => 1)
+      }
+
+    if @problem.save
+      flash[:success] = "Problem created!"
+      redirect_to @problem
+    else
+      @course = course
+      render 'new'
+    end
+  end
+
   def new
     @course = Course.find(params[:course_id])
     @problem = Problem.new  
   end
 
+  def new_tex
+    @course = Course.find(params[:course_id])
+    @problem = Problem.new
+  end 
+  
   def update
     @problem = Problem.find(params[:id])
 
@@ -70,5 +103,17 @@ class ProblemsController < ApplicationController
         redirect_to root_path
         return false
       end 
+    end
+
+    def find_name(text)
+      statement = text.scan(/\\begin{name}(.*)\\end{name}/im)
+      text = statement.map{ |x| x[0]}.join
+      return text.chomp
+    end
+
+    def find_statement(text)
+      statement = text.scan(/\\begin{statement}(.*)\\end{statement}/im)
+      text = statement.map{ |x| x[0]}.join
+      return text.chomp
     end
 end
