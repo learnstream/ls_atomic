1c399ab01009593d6696b61d2f2d989b98131afd
Camille Marvin <camille.marvin@gmail.com>
2011-06-07 20:48:32 -0700

    forced to commit before pulll

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,6 @@
 
 ActiveRecord::Schema.define(:version => 20110604005229) do
 
-  create_table "component_tests", :force => true do |t|
-    t.integer  "component_id"
-    t.integer  "test_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "component_tests", ["component_id"], :name => "index_component_tests_on_component_id"
-  add_index "component_tests", ["test_id", "component_id"], :name => "index_component_tests_on_test_id_and_component_id", :unique => true
-  add_index "component_tests", ["test_id"], :name => "index_component_tests_on_test_id"
-
   create_table "components", :force => true do |t|
     t.string   "name"
     t.text     "description"
@@ -145,19 +134,6 @@ ActiveRecord::Schema.define(:version => 20110604005229) do
     t.integer  "order_number"
     t.integer  "problem_id"
   end
-
-  create_table "tests", :force => true do |t|
-    t.integer  "problem_id"
-    t.string   "steps"
-    t.string   "question"
-    t.text     "answer_input"
-    t.string   "answer"
-    t.text     "answer_output"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "tests", ["problem_id"], :name => "index_tests_on_problem_id"
 
   create_table "user_sessions", :force => true do |t|
     t.datetime "created_at"
