fcefbe548c275d9a36b401be12a822e80ef0bef0
Camille Marvin <camille.marvin@gmail.com>
2011-06-10 18:58:14 -0700

    changed how study controller pulls up quizzes

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -5,23 +5,40 @@ class StudyController < ApplicationController
 
   def index
     @course = Course.find(params[:course_id])
-    @quiz = nil
-    
-    while @quiz.nil?
-      @memory = current_user.memories_due(@course).first 
 
+    @quiz = nil
+    count = 0
+    while @quiz.nil?
+
+      @memory = current_user.memories_due(@course).order[count]
       if @memory.nil? 
         render 'index'
+        count = 0
         return
       end
-      
-      @quiz = @memory.component.quizzes.first
-      
-      if @quiz.nil?
-        @memory.view(0)
-      end
+
+      @quiz = @memory.component.quizzes.sample
+      count += 1
     end
 
     redirect_to @quiz
+
+#while @quiz.nil?
+#  @memory = current_user.memories_due(@course).first 
+#  if @memory.nil? 
+#    render 'index'
+#    return
+#  end
+#  
+#  @quiz = @memory.component.quizzes.first
+#  
+#  if @quiz.nil?
+#    @memory.view(0)
+#  end
+#end
+#redirect_to @quiz
+
   end
 end
+
+
