1ddc79131befc53e22014c34209eb0cf70943f49
Camille Marvin <camille.marvin@gmail.com>
2011-06-21 11:10:48 -0700

    added breadcrumb links for all reasonable pages. For some reason the top level isn't linking right now though

--- config/initializers/breadcrumbs.rb
+++ config/initializers/breadcrumbs.rb
@@ -6,6 +6,7 @@ Gretel::Crumbs.layout do
     link "Home", root_path
   end
 
+  # Course
   crumb :courses do
     link "Courses", courses_path
   end
@@ -15,38 +16,62 @@ Gretel::Crumbs.layout do
     parent :courses
   end
 
-  crumb :course_edit do |course|
-    link "Edit", course_edit_path(course)
-    parent :courses, course
+  crumb :new_course do
+    link "New Course", new_course_path
+    parent :courses
   end
 
-  crumb :course_lessons do |course|
+  crumb :student_status do |course|
+    link "Student Status", student_status_course_path(course)
+    parent :course, course
+  end
+
+  # Lessons
+  crumb :lessons do |course|
     link "Lessons", course_lessons_path(course)
     parent :course, course
   end
 
-  crumb :edit_course_lesson do |lesson|
-    link lambda {|lesson| "Edit Lesson : #{lesson.name}"}, edit_course_lesson_path(lesson.course, lesson)
-    parent :course_lessons, lesson.course
+  crumb :new_lesson do |course|
+    link "New Lesson", new_course_lesson_path(course)
+    parent :lessons, course
   end
-  
-  # crumb :projects do
-  #   link "Projects", projects_path
-  # end
-  
-  # crumb :project do |project|
-  #   link lambda { |project| "#{project.name} (#{project.id.to_s})" }, project_path(project)
-  #   parent :projects
-  # end
-  
-  # crumb :project_issues do |project|
-  #   link "Issues", project_issues_path(project)
-  #   parent :project, project
-  # end
-  
-  # crumb :issue do |issue|
-  #   link issue.name, issue_path(issue)
-  #   parent :project_issues, issue.project
-  # end
 
+  crumb :edit_lesson do |lesson|
+    link lambda {|lesson| "Edit Lesson : #{lesson.name}"}, edit_course_lesson_path(lesson.course, lesson)
+    parent :lessons, lesson.course
+  end
+
+  # Components
+  crumb :components do |course|
+    link "Components", course_components_path(course)
+    parent :course, course
+  end
+
+  crumb :component do |component|
+    link lambda {|component| "#{component.name}"}, course_component_path(component.course,component)
+    parent :components, component.course
+  end
+
+  crumb :new_component do |course|
+    link "New Lesson", new_course_component_path(course)
+    parent :components, course
+  end
+
+  crumb :edit_component do |component|
+    link "Edit", edit_course_component_path(component.course, component)
+    parent :component, component
+  end
+
+  # Quizzes
+  crumb :quizzes do |course|
+    link "Exercises", course_quizzes_path(course)
+    parent :course, course
+  end
+
+  crumb :new_quiz do |course|
+    link "New Exercise", new_course_quiz_path(course)
+    parent :quizzes, course
+  end
+ 
 end
