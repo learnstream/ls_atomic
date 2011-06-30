ab9cb3029af3826c13f8b6704f5574fb90dd479b
Ryan Muller <cognitionmachine@gmail.com>
2011-06-29 18:51:05 -0700

    created associations

--- app/models/lesson_component.rb
+++ app/models/lesson_component.rb
@@ -1,2 +1,5 @@
 class LessonComponent < ActiveRecord::Base
+  belongs_to :lesson
+  belongs_to :component
+  belongs_to :event
 end
--- spec/models/lesson_component_spec.rb
+++ spec/models/lesson_component_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe LessonComponent do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @course = Factory(:course)
+    @lesson = Factory(:lesson, :course => @course)
+    @component = Factory(:component, :course => @course)
+    @lesson_component = LessonComponent.create!(:lesson_id => @lesson.id, 
+                                            :component_id => @component.id)
+    @lesson_component.reload
+  end
+
+  it "should have a lesson method" do
+    @lesson_component.should respond_to(:lesson)
+  end
+
+  it "should have the right lesson" do
+    @lesson_component.lesson.should == @lesson
+  end
+
+  it "should have a component method" do
+    @lesson_component.should respond_to(:component)
+  end
+
+  it "should have the right component" do
+    @lesson_component.component.should == @component
+  end
+
 end
