8afb5c458dc660d584c605431ab16a4f7489ced2
Neal Pisenti <npisenti@gmail.com>
2011-06-30 17:01:19 -0700

    removed the text limit for quiz questions, to hopeuflly play nice on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110624181838) do
+ActiveRecord::Schema.define(:version => 20110630234844) do
 
   create_table "answers", :force => true do |t|
     t.string   "text"
