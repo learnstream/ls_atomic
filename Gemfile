535c706a252a16da8d621125879f3addd9c27d75
Ryan Muller <cognitionmachine@gmail.com>
2011-06-30 20:01:22 -0700

    Revert "deprecation warnings gone, but gem dependencies unhappy"
    This reverts commit e59527cdada4c03e9046553f6d5e816b06c0a529.

--- Gemfile
+++ Gemfile
@@ -25,11 +25,12 @@ group :development, :test do
 end
 
 group :development do
-  gem 'rspec-rails', '>= 2.5.0'
+  gem 'rspec-rails', '2.5.0'
 end
 
 group :test do
-  gem 'rspec', '>= 2.5.0'
+  gem 'webrat', '0.7.1'
+  gem 'rspec', '2.5.0'
   gem 'spork', '0.9.0.rc5'
   gem 'factory_girl_rails', '1.0'
 end
---
+++ spec/requests/add_video_to_components_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "AddVideoToComponents" do
+
+  before(:each) do
+    @course = Factory(:course)
+    @teacher = Factory(:user)
+    @teacher.enroll_as_teacher!(@course)
+    @component = Factory(:component, :course => @course)
+    integration_sign_in(@teacher)
+  end
+
+  it "should add a video to a component" do
+    visit edit_course_component_path(@course, @component)
+    within "#new_video" do
+      fill_in "Url", :with => "http://www.youtube.com/watch?v=nIBfNsPDw1I&feature=feedrec_grec_index" 
+      fill_in "Start time", :with => 0
+      fill_in "End time", :with => 60
+      fill_in "Name", :with => "Spaghetti"
+      fill_in "Description", :with => "Om nom nom"
+      click_button "Add video" 
+    end
+    page.should have_content("Video created!")
+  end 
+end
