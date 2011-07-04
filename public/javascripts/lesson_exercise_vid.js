7bfe46714213c2d0391227260647b818ab84cce9
Ryan Muller <cognitionmachine@gmail.com>
2011-07-04 15:49:38 -0700

    only try to embed video if video data is provided

--- public/javascripts/lesson_exercise_vid.js
+++ public/javascripts/lesson_exercise_vid.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    console.log("asdf");
-    loadAndPlayVideo(getYoutubeID($("#player").attr("data-url"), "v"), $("#player").attr("data-starttime"), $("#player").attr("data-endtime"), "player", 0);
+    if($("#player").attr('data-url') != "") 
+      loadAndPlayVideo(getYoutubeID($("#player").attr("data-url"), "v"), $("#player").attr("data-starttime"), $("#player").attr("data-endtime"), "player", 0);
 });
