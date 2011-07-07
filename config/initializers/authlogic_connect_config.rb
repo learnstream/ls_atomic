afcd453d623623e5eaec0da2207c779cac39af17
Ryan Muller <cognitionmachine@gmail.com>
2011-07-07 11:30:22 -0700

    missed a line in the production config

--- config/initializers/authlogic_connect_config.rb
+++ config/initializers/authlogic_connect_config.rb
@@ -3,10 +3,12 @@ when "development"
   AuthlogicConnect.config = YAML.load_file("config/authlogic.yml")
 when "production"
   AuthlogicConnect.config = {
-    :facebook => {
-      :key => ENV["CONNECT_FACEBOOK_KEY"],
-      :secret => ENV["CONNECT_FACEBOOK_SECRET"],
-      :label => "Facebook"
+    :connect => {
+      :facebook => {
+        :key => ENV["CONNECT_FACEBOOK_KEY"],
+        :secret => ENV["CONNECT_FACEBOOK_SECRET"],
+        :label => "Facebook"
+      }
     }
   }
 end
