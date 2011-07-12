b6f569b1f39d4d990a2459b4c7ac8faeb277f775
Ryan Muller <cognitionmachine@gmail.com>
2011-07-12 11:24:16 -0700

    cleaned up study response page a bit

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -1,2 +1,34 @@
 module ResponsesHelper
+  def user_answer(response)
+    if response.quiz.answer_type == 'multi'
+      show_multi_answer(response.answer, response.quiz.answer_input)
+    elsif response.quiz.answer_type == 'check'
+      show_check_answer(response.answer, response.quiz.answer_input)
+    else
+      response.answer
+    end
+  end
+
+  def quiz_answer(response)
+    if response.quiz.answer_type == 'multi'
+      show_multi_answer(response.quiz.answers.first.text, response.quiz.answer_input)
+    elsif response.quiz.answer_type == 'check'
+      show_check_answer(response.quiz.answers.first.text, response.quiz.answer_input)
+    else
+      response.quiz.answers.first.text
+    end
+  end
+
+  def show_multi_answer(answer, answer_input)
+    ActiveSupport::JSON.decode(answer_input)["choices"][answer.to_i]
+  end
+  
+  def show_check_answer(answer, answer_input)
+    answers = answer.split("")
+    str = ""
+    answers.each do |ans|
+      str += ActiveSupport::JSON.decode(answer_input)["choices"][ans.to_i] + "\n"
+    end
+    str
+  end
 end
--- app/helpers/study_helper.rb
+++ app/helpers/study_helper.rb
@@ -1,17 +1,2 @@
 module StudyHelper
-
-  # Our answer field contains the number of the response, not the actual answer
-  def showMultiAnswer(answer, answer_input)
-    ActiveSupport::JSON.decode(answer_input)["choices"][answer.to_i]
-  end
-  
-  def showCheckAnswer(answer, answer_input)
-    answers = answer.split("")
-    str = ""
-    answers.each do |ans|
-      str += ActiveSupport::JSON.decode(answer_input)["choices"][ans.to_i] + "\n"
-    end
-    str
-  end
-
 end
