26dee82453f3a3758a1829a17e88dae8e4f74dd9
Neal Pisenti <npisenti@gmail.com>
2011-07-12 22:31:26 -0700

    removed and tidied up some files for seed data

--- db/fixtures/05_calculus_lesson_eventss.rb
+++
@@ -1,9 +0,0 @@
-# DO NOT MODIFY THIS FILE, it was auto-generated.
-#
-# Date: 2011-07-12 18:24:54 -0700
-# Seeding Event
-# Written with the command:
-#
-#   ./script/SeedDataWriter 
-#
-Event.seed(:id
