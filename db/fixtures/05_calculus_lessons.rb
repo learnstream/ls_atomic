60938b290a404fd64dac4da26a5c5c58c85201b1
Neal Pisenti <npisenti@gmail.com>
2011-07-13 14:47:38 -0700

    fixed a to_s bug in the new hash id_encode method

--- db/fixtures/05_calculus_lessons.rb
+++ db/fixtures/05_calculus_lessons.rb
@@ -1,24 +1,24 @@
 # DO NOT MODIFY THIS FILE, it was auto-generated.
 #
-# Date: 2011-07-13 14:00:37 -0700
+# Date: 2011-07-13 14:43:08 -0700
 # Seeding Lesson
 # Written with the command:
 #
 #   ./script/SeedDataWriter 
 #
 Lesson.seed(:id,
-  {:id=>51371309, :course_id=>2, :name=>"Common Algebra Mistakes", :order_number=>0, :component_tokens=>""},
-  {:id=>51371310, :course_id=>2, :name=>"Review of Trigonometric, Logarithmic, and Exponential Functions", :order_number=>1, :component_tokens=>""},
-  {:id=>51371311, :course_id=>2, :name=>"Functions and Transformations of Functions", :order_number=>2, :component_tokens=>""},
-  {:id=>51371312, :course_id=>2, :name=>"Limit Definition of the Derivative", :order_number=>4, :component_tokens=>"145761641,145761642,145761643,145761644,145761645"},
-  {:id=>51371313, :course_id=>2, :name=>"The Chain Rule", :order_number=>5, :component_tokens=>"145754638,145754639"},
-  {:id=>51371314, :course_id=>2, :name=>"The Product Rule", :order_number=>6, :component_tokens=>"298898358,298898359"},
-  {:id=>51371315, :course_id=>2, :name=>"The Quotient Rule", :order_number=>7, :component_tokens=>"298898360,298898361,298898362"},
-  {:id=>51371316, :course_id=>2, :name=>"L'Hospital's Rule", :order_number=>8, :component_tokens=>"145761638,145761639,145761640,298902353"},
-  {:id=>51371317, :course_id=>2, :name=>"The First Derivative: Maxima and Minima", :order_number=>9, :component_tokens=>"145756647,145757638,145757639,145757640,145757641,145757642,145757643,145757644,145757645,145757646,145757647,145758638,145758639,145758640"},
-  {:id=>163348853, :course_id=>2, :name=>"Concavity and the Second Derivative Test", :order_number=>10, :component_tokens=>"298899356,298899357,298899358,298899359,298899360,298899361,298899362,298900353,298900354,298900355,298900356,298900357"},
-  {:id=>163348854, :course_id=>2, :name=>"Computing Integrals by Substitution", :order_number=>11, :component_tokens=>"145754643,145754644,298901359"},
-  {:id=>163348855, :course_id=>2, :name=>"Integration by Parts", :order_number=>12, :component_tokens=>"145760645,145760646,145760647,298901360,298901361,298901362"},
-  {:id=>163348856, :course_id=>2, :name=>"Computing Limits", :order_number=>3, :component_tokens=>"298902360,298902361,298902362,298903353,298903354,298903355,298903356,298903357,298903358,298903359,298903360,298903361,298903362"}
+  {:id=>228348225, :course_id=>2, :name=>"Common Algebra Mistakes", :order_number=>0, :component_tokens=>""},
+  {:id=>59165149, :course_id=>2, :name=>"Review of Trigonometric, Logarithmic, and Exponential Functions", :order_number=>1, :component_tokens=>""},
+  {:id=>1039797, :course_id=>2, :name=>"Functions and Transformations of Functions", :order_number=>2, :component_tokens=>""},
+  {:id=>53245379, :course_id=>2, :name=>"Limit Definition of the Derivative", :order_number=>4, :component_tokens=>"125280352,148926907,256827288,284701906,70618010"},
+  {:id=>119208138, :course_id=>2, :name=>"The Chain Rule", :order_number=>5, :component_tokens=>"60218940,311211863"},
+  {:id=>25817111, :course_id=>2, :name=>"The Product Rule", :order_number=>6, :component_tokens=>"254483068,45504393"},
+  {:id=>253777357, :course_id=>2, :name=>"The Quotient Rule", :order_number=>7, :component_tokens=>"265435821,123680427,167385158"},
+  {:id=>125953870, :course_id=>2, :name=>"L'Hospital's Rule", :order_number=>8, :component_tokens=>"185096481,202515598,221665214,272398396"},
+  {:id=>184447994, :course_id=>2, :name=>"The First Derivative: Maxima and Minima", :order_number=>9, :component_tokens=>"53253080,76913966,33622791,177966794,11384276,30159322,287577748,210939754,175814943,185102778,205059657,85882939,229949979,201434753"},
+  {:id=>145714453, :course_id=>2, :name=>"Concavity and the Second Derivative Test", :order_number=>10, :component_tokens=>"166442372,50709657,33752398,132892976,177344975,28719449,302968560,88666458,126107999,144175650,268709690,35524757"},
+  {:id=>260189696, :course_id=>2, :name=>"Computing Integrals by Substitution", :order_number=>11, :component_tokens=>"56688196,285712046,39602143"},
+  {:id=>13449216, :course_id=>2, :name=>"Integration by Parts", :order_number=>12, :component_tokens=>"304595141,233173976,35255298,95526097,134098192,276849960"},
+  {:id=>74921373, :course_id=>2, :name=>"Computing Limits", :order_number=>3, :component_tokens=>"77618437,78902713,5434007,269295645,286201053,311913969,306976102,153739952,31408008,28314170,8899444,180413188,194030908"}
 )
 # End auto-generated file.
--- db/fixtures/09_physics_lessons.rb
+++ db/fixtures/09_physics_lessons.rb
@@ -1,25 +1,25 @@
 # DO NOT MODIFY THIS FILE, it was auto-generated.
 #
-# Date: 2011-07-13 14:00:37 -0700
+# Date: 2011-07-13 14:43:08 -0700
 # Seeding Lesson
 # Written with the command:
 #
 #   ./script/SeedDataWriter 
 #
 Lesson.seed(:id,
-  {:id=>189477489, :course_id=>1, :name=>"Introduction to Position, Velocity, and Acceleration", :order_number=>2, :component_tokens=>"74852082,74852083,74852084,74852085,74852086,74852087,74853078,74853079,74853080,74853081,74853082,76175984,76175985"},
-  {:id=>189477490, :course_id=>1, :name=>"Example with Position, Velocity, and Acceleration", :order_number=>3, :component_tokens=>""},
-  {:id=>189477491, :course_id=>1, :name=>"Introduction to Vectors", :order_number=>0, :component_tokens=>"30862454,30862455,30862456,30862457,30862459,76175981"},
-  {:id=>189477492, :course_id=>1, :name=>"Multiplication of Vectors", :order_number=>1, :component_tokens=>"30862461,30862462,76175979,76175980,76175982,76175983,76176978"},
-  {:id=>189477493, :course_id=>1, :name=>"Conservation of momentum example - Ice skater", :order_number=>12, :component_tokens=>""},
-  {:id=>189477494, :course_id=>1, :name=>"Newton's 1st and 2nd Laws", :order_number=>7, :component_tokens=>"74849080,74850085,76172978,76172979,76174986,76176984,76176985"},
-  {:id=>189477495, :course_id=>1, :name=>"Newton's 3rd Law", :order_number=>8, :component_tokens=>"76174987"},
-  {:id=>189477496, :course_id=>1, :name=>"Example of Newton's Laws with Hanging Mass", :order_number=>9, :component_tokens=>""},
-  {:id=>189477497, :course_id=>1, :name=>"Introduction to Weight", :order_number=>10, :component_tokens=>"76172980,76172981,76175986,76175987"},
-  {:id=>39452253, :course_id=>1, :name=>"Example - Block sliding down frictionless plane", :order_number=>11, :component_tokens=>""},
-  {:id=>39452254, :course_id=>1, :name=>"Kinematics motion in 3-D space", :order_number=>5, :component_tokens=>""},
-  {:id=>39452255, :course_id=>1, :name=>"Example - Projectile motion in a plane", :order_number=>4, :component_tokens=>""},
-  {:id=>39452256, :course_id=>1, :name=>"Example - Monkey hunting and projectile motion", :order_number=>6, :component_tokens=>""},
-  {:id=>39452257, :course_id=>1, :name=>"Centripetal forces and accelerations", :order_number=>13, :component_tokens=>"74849086,74853087,74854078,74854079,76176979,76176980,76176981,76176982"}
+  {:id=>231263311, :course_id=>1, :name=>"Introduction to Position, Velocity, and Acceleration", :order_number=>2, :component_tokens=>"175676994,39238552,206575253,300842059,164689172,242130479,225114560,255145840,232707204,67391558,140167706,280406973,135763260"},
+  {:id=>1803529, :course_id=>1, :name=>"Example with Position, Velocity, and Acceleration", :order_number=>3, :component_tokens=>""},
+  {:id=>247008306, :course_id=>1, :name=>"Introduction to Vectors", :order_number=>0, :component_tokens=>"193188329,257893870,273685203,30111287,134890592,230756916"},
+  {:id=>139341695, :course_id=>1, :name=>"Multiplication of Vectors", :order_number=>1, :component_tokens=>"267851257,164065156,230617014,35568486,108222687,235377666,169749181"},
+  {:id=>70651852, :course_id=>1, :name=>"Conservation of momentum example - Ice skater", :order_number=>12, :component_tokens=>""},
+  {:id=>300653055, :course_id=>1, :name=>"Newton's 1st and 2nd Laws", :order_number=>7, :component_tokens=>"168062596,198863220,139348573,261176174,271940739,156328861,178577831"},
+  {:id=>109254327, :course_id=>1, :name=>"Newton's 3rd Law", :order_number=>8, :component_tokens=>"203429119"},
+  {:id=>216952576, :course_id=>1, :name=>"Example of Newton's Laws with Hanging Mass", :order_number=>9, :component_tokens=>""},
+  {:id=>42035434, :course_id=>1, :name=>"Introduction to Weight", :order_number=>10, :component_tokens=>"69738265,49528888,281470334,295617994"},
+  {:id=>197485137, :course_id=>1, :name=>"Example - Block sliding down frictionless plane", :order_number=>11, :component_tokens=>""},
+  {:id=>66406803, :course_id=>1, :name=>"Kinematics motion in 3-D space", :order_number=>5, :component_tokens=>""},
+  {:id=>253299482, :course_id=>1, :name=>"Example - Projectile motion in a plane", :order_number=>4, :component_tokens=>""},
+  {:id=>199183808, :course_id=>1, :name=>"Example - Monkey hunting and projectile motion", :order_number=>6, :component_tokens=>""},
+  {:id=>43385732, :course_id=>1, :name=>"Centripetal forces and accelerations", :order_number=>13, :component_tokens=>"90112461,86388434,203625267,30329891,186216977,23137634,226492683,291668703"}
 )
 # End auto-generated file.
