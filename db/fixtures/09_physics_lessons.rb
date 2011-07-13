06eee0b2915be2a1be2c157c5736a0b11fad5918
Neal Pisenti <npisenti@gmail.com>
2011-07-13 12:00:11 -0700

    added physics lesson data, added an event_token attr_reader to quizzes

---
+++ db/fixtures/09_physics_lessons.rb
@@ -0,0 +1,25 @@
+# DO NOT MODIFY THIS FILE, it was auto-generated.
+#
+# Date: 2011-07-13 11:57:59 -0700
+# Seeding Lesson
+# Written with the command:
+#
+#   ./script/SeedDataWriter 
+#
+Lesson.seed(:id,
+  {:id=>189477489, :course_id=>1, :name=>"Introduction to Position, Velocity, and Acceleration", :order_number=>2, :component_tokens=>"74852082,74852083,74852084,74852085,74852086,74852087,74853078,74853079,74853080,74853081,74853082,76175984,76175985"},
+  {:id=>189477490, :course_id=>1, :name=>"Example with Position, Velocity, and Acceleration", :order_number=>3, :component_tokens=>""},
+  {:id=>189477491, :course_id=>1, :name=>"Introduction to Vectors", :order_number=>0, :component_tokens=>"30862454,30862455,30862456,30862457,30862459,76175981"},
+  {:id=>189477492, :course_id=>1, :name=>"Multiplication of Vectors", :order_number=>1, :component_tokens=>"30862461,30862462,76175979,76175980,76175982,76175983,76176978"},
+  {:id=>189477493, :course_id=>1, :name=>"Conservation of momentum example - Ice skater", :order_number=>12, :component_tokens=>""},
+  {:id=>189477494, :course_id=>1, :name=>"Newton's 1st and 2nd Laws", :order_number=>7, :component_tokens=>"74849080,74850085,76172978,76172979,76174986,76176984,76176985"},
+  {:id=>189477495, :course_id=>1, :name=>"Newton's 3rd Law", :order_number=>8, :component_tokens=>"76174987"},
+  {:id=>189477496, :course_id=>1, :name=>"Example of Newton's Laws with Hanging Mass", :order_number=>9, :component_tokens=>""},
+  {:id=>189477497, :course_id=>1, :name=>"Introduction to Weight", :order_number=>10, :component_tokens=>"76172980,76172981,76175986,76175987"},
+  {:id=>39452253, :course_id=>1, :name=>"Example - Block sliding down frictionless plane", :order_number=>11, :component_tokens=>""},
+  {:id=>39452254, :course_id=>1, :name=>"Kinematics motion in 3-D space", :order_number=>5, :component_tokens=>""},
+  {:id=>39452255, :course_id=>1, :name=>"Example - Projectile motion in a plane", :order_number=>4, :component_tokens=>""},
+  {:id=>39452256, :course_id=>1, :name=>"Example - Monkey hunting and projectile motion", :order_number=>6, :component_tokens=>""},
+  {:id=>39452257, :course_id=>1, :name=>"Centripetal forces and accelerations", :order_number=>13, :component_tokens=>"74849086,74853087,74854078,74854079,76176979,76176980,76176981,76176982"}
+)
+# End auto-generated file.
