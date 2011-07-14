fac241ae487a55635ecf308e5115c8e2adcf2f66
Camille Marvin <camille.marvin@gmail.com>
2011-07-13 17:29:19 -0700

    working on adding

--- app/models/lesson_component.rb
+++ app/models/lesson_component.rb
@@ -6,4 +6,15 @@ class LessonComponent < ActiveRecord::Base
   validates :lesson_id, :presence => true
   validates :component_id, :presence => true
   validates :component_id, :uniqueness => { :scope => :lesson_id }
+
+  after_create :unlock_memories_for_students
+
+  def unlock_memories_for_students
+    @students = lesson.course.students
+    @students.each do |student|
+      status = student.lesson_statuses.find_by_lesson_id(lesson)
+      if(status != -1)
+        student.
+    end
+  end
 end
