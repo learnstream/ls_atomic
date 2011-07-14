1656f5f6887f9cfe1a31835a1bdda3ca320925d1
Camille Marvin <camille.marvin@gmail.com>
2011-07-14 12:56:33 -0700

    displaying input options for multiple choice quizzes in teacher view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,39 @@ module ApplicationHelper
     end
     link_to_function(name, "add_fields(this, '#{association}', '#{escape_javascript(fields)}')", :id => "addanswerbutton")
   end
+
+  # For showing multiple choice quizzes and answers
+  #
+  def quiz_answer(quiz)
+    if quiz.answer_type == 'multi'
+      show_multi_answer(quiz.answers.first.text, quiz.answer_input)
+    elsif quiz.answer_type == 'check'
+      show_check_answer(quiz.answers.first.text, quiz.answer_input)
+    else
+      quiz.answers.first.text
+    end
+  end
+
+  def show_multi_answer(answer, answer_input)
+    ActiveSupport::JSON.decode(answer_input)["choices"][answer.to_i]
+  end
+  
+  def show_check_answer(answer, answer_input)
+    answers = answer.split("")
+    str = ""
+    answers.each do |ans|
+      str += ActiveSupport::JSON.decode(answer_input)["choices"][ans.to_i] + "\n"
+    end
+    str
+  end
+
+  def show_multi_choices(answer_input)
+    str = '<ul>'
+    ActiveSupport::JSON.decode(answer_input)["choices"].each do |choice| 
+      str += '<li>' + choice + '</li>'
+    end
+    str += '</ul>'
+    str
+  end
+
 end
